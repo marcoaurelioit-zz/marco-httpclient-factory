e0d1545ea8841a35f66595f1b9076dee0ff018fa
Marco Aurélio <maureliobrasil@gmail.com>
2019-02-08 17:39:07 -0200

    ajustes

---
+++ src/Marco.Http.Client.AspNetCore/Microsoft.Extensions.DependencyInjection/MarcoAspNetCoreHttpClientSeviceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Marco.Http.Client.Abstractions;
+using Marco.Http.Client.AspNetCore;
+using System;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+   public static class MarcoAspNetCoreHttpClientSeviceCollectionExtensions
+    {
+        public static IServiceCollection AddAspNetCoreHttpClientFactory(this IServiceCollection services)
+        {
+            if (services == null)
+                throw new ArgumentNullException(nameof(services));
+
+            services.AddScoped<IMarcoHttpClientFactory, MarcoHttpClientFactory>();
+
+            return services;
+        }
+    }
+}
